53f02b24a645ada8f1b443f26577060973cd905e
stonar96 <minecraft.stonar96@gmail.com>
2021-11-09 01:54:47 +0100

    Add the project

---
+++ NmsTestPlugin/src/main/java/com/vanillage/nmstestplugin/NmsTestPlugin.java
@@ -0,0 +1,72 @@
+package com.vanillage.nmstestplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_17_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_17_R1.command.CraftCommandMap;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.minecraft.commands.Commands;
+
+public final class NmsTestPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        saveResource("README.txt", false);
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+        // saveConfig();
+        // Initialize stuff.
+        // Register events.
+        registerCommands();
+        getLogger().info(getDescription().getFullName() + " enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        unregisterCommands();
+        // Cleanup stuff.
+        getLogger().info(getDescription().getFullName() + " disabled");
+    }
+
+    public void onReload() {
+        // Cleanup stuff.
+        saveDefaultConfig();
+        reloadConfig();
+        getConfig().options().copyDefaults(true);
+        // saveConfig();
+        // Initialize stuff.
+        getLogger().info(getDescription().getFullName() + " reloaded");
+    }
+
+    public void registerCommands() {
+        ((CraftServer) getServer()).getServer().vanillaCommandDispatcher.getDispatcher().register(Commands.literal("nmstestplugin").requires(c -> c.getBukkitSender().hasPermission("nmstestplugin.command.nmstestplugin")).then(Commands.literal("reload").requires(c -> c.getBukkitSender().hasPermission("nmstestplugin.command.nmstestplugin.reload")).executes(c -> {
+            onReload();
+            sendMessage(c.getSource().getBukkitSender(), "reload");
+            return 1;
+        })).then(Commands.literal("text").requires(c -> c.getBukkitSender().hasPermission("nmstestplugin.command.nmstestplugin.text")).executes(c -> {
+            sendMessage(c.getSource().getBukkitSender(), "text");
+            return 1;
+        })));
+    }
+
+    public void unregisterCommands() {
+        Command command = ((CraftServer) getServer()).getCommandMap().getCommand("minecraft:nmstestplugin");
+
+        if (command != null) {
+            command.unregister(((CraftServer) getServer()).getCommandMap());
+        }
+
+        if (((CraftCommandMap) ((CraftServer) getServer()).getCommandMap()).getKnownCommands().remove("nmstestplugin", ((CraftCommandMap) ((CraftServer) getServer()).getCommandMap()).getKnownCommands().remove("minecraft:nmstestplugin"))) {
+            ((CraftServer) getServer()).getServer().getCommands().getDispatcher().getRoot().removeCommand("nmstestplugin");
+        }
+
+        ((CraftServer) getServer()).getServer().vanillaCommandDispatcher.getDispatcher().getRoot().removeCommand("nmstestplugin");
+        ((CraftServer) getServer()).getServer().getCommands().getDispatcher().getRoot().removeCommand("minecraft:nmstestplugin");
+        getServer().getOnlinePlayers().forEach(p -> ((CraftServer) getServer()).getServer().getCommands().sendCommands(((CraftPlayer) p).getHandle()));
+    }
+
+    private void sendMessage(CommandSender sender, String messageName) {
+        sender.sendMessage(getConfig().getStringList("messages." + messageName).toArray(new String[0]));
+    }
+}
---
+++ NmsTestPlugin/src/main/resources/README.txt
@@ -0,0 +1,16 @@
+Commands and required permissions:
+All commands require at least the permissions 'minecraft.command.nmstestplugin' and 'nmstestplugin.command.nmstestplugin'.
+
+Command:
+/nmstestplugin reload
+Description:
+Reloads the config.yml.
+Permissions:
+nmstestplugin.command.nmstestplugin.reload
+
+Command:
+/nmstestplugin text
+Description:
+Displays text.
+Permissions:
+nmstestplugin.command.nmstestplugin.text
